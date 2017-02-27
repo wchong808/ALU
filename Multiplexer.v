8e40d185ee69af3ca8ab03a0dd8cd26d157fdcfe
Cindy Chong <Cindy Chong>
2017-02-26 22:32:11 -1000

    finished multiplexer

---
+++ Multiplexer.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/11/2016 09:46:12 AM
+// Design Name: 
+// Module Name: Multiplexer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Multiplexer(
+    input [1:0] selMUX,
+    output [3:0] M
+    );
+endmodule
